8585aa713e4e0a344a42b0849124555099c72e59
s00190029 <mrdaylight@protonmail.com>
2020-10-29 19:24:28 +0000

    class & print of its values

--- app/src/main/java/edu/rdonoghue/resistor/MainActivity.java
+++ app/src/main/java/edu/rdonoghue/resistor/MainActivity.java
@@ -3,12 +3,28 @@ package edu.rdonoghue.resistor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // link
+        hello = findViewById(R.id.tv_hello);
+
+        resistorColor red = new resistorColor();
+        red.band1 = 2;
+
+        hello.setText( String.valueOf(red.band1));
+
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/edu/rdonoghue/resistor/resistorColor.java
@@ -0,0 +1,12 @@
+package edu.rdonoghue.resistor;
+
+
+public class resistorColor {
+    int band1;
+    int band2;
+    int multiplier;
+}
+
+
+
+
